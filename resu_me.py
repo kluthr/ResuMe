056efb9b4957576a07dc76387622c021875d28d6
kluthr <becky@trapit.com>
2017-04-17 17:17:55 -0700

    Add search and cli

---
+++ resu_me.py
@@ -0,0 +1,10 @@
+
+
+
+# Use Google search API to grab a bunch of PDFs of resumes
+# Use something (PyPDF2??) to scrape the resumes
+# add data to db?
+# use nlg to create a new resume
+# swap out keywords for other keywords
+
+
